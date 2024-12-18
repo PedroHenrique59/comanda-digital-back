1d9550355a91a8168617821402ec19b63ccc5b00
Pedro Henrique <pedro.walter@cnj.jus.br>
2024-12-18 17:14:49 -0300

    Criando Projeto com a primeira entidade e conexão com banco de dados e versionamento

---
+++ src/main/java/model/Mesa.java
@@ -0,0 +1,20 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Mesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numero;
+
+    private String descricao;
+}
